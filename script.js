019be63af342792ca454e0eed8b0178a5683e1dc
Kagheni <Kaghenimbale@gmail.com>
2023-02-14 08:30:17 +0200

    Add the Add book function in js

---
+++ script.js
@@ -0,0 +1,42 @@
+const form = document.getElementById('form');
+const inputBook = document.getElementById('book');
+const inputAuthor = document.getElementById('author');
+const Container = document.getElementById('container');
+
+// Add Books
+
+const addBook = () => {
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const authorbook = inputAuthor.value;
+    const titlebook = inputBook.value;
+    const itemlist = document.createElement('div');
+    itemlist.className = 'Itemlist';
+
+    const obj = {
+      Title: titlebook,
+      Author: authorbook,
+    };
+
+    const book = `
+    <ul class=''>
+    <li>${obj.Title}</li>
+    <li>${obj.Author}</li>
+    </ul>
+    `;
+    const removeBtn = document.createElement('button');
+    removeBtn.className = 'remove';
+    removeBtn.id = 'removeBtn';
+    removeBtn.innerText = 'Remove';
+
+    itemlist.innerHTML = book;
+    itemlist.appendChild(removeBtn);
+    Container.append(itemlist);
+
+    const result = JSON.stringify(obj);
+    localStorage.setItem('data', result);
+  });
+};
+
+addBook();
