5780b89b5ebb8ffd4bc3fed9b45e122bc2edf5a8
Kagheni <Kaghenimbale@gmail.com>
2023-02-15 19:36:51 +0200

    Fix bugs on the js file

--- script.js
+++ script.js
@@ -3,80 +3,73 @@ const authorInput = document.getElementById('author');
 const form = document.getElementById('form');
 const container = document.getElementById('container');
 const bookskey = 'books';
-let deleteBtns = document.querySelectorAll('#remove');
 
 class Book {
-  constructor(title, author, id) {
-    this.author = author;
-    this.title = title;
-    if (id) {
-      this.id = id;
-    } else {
-      this.id = 1;
-    }
+  constructor(books) {
+    this.books = books;
   }
 
-  AddBook() {
-    const book = document.createElement('li');
-    book.innerHTML = `
+  displayBooks() {
+    container.innerHTML = '';
+    this.books.forEach((book) => {
+      const bookElement = this.getBookElement(book);
+
+      container.appendChild(bookElement);
+    });
+
+    this.removeBook();
+  }
+
+  getBookElement = ({ title, author, id }) => {
+    const newbook = document.createElement('li');
+    newbook.innerHTML = `
     <div class="description">
-    <h2>${this.title}</h2>
+    <h2>${title}</h2>
     by
-    <h3>${this.author}</h3>
+    <h3>${author}</h3>
     </div>
-    <button data-id=${this.id} id='remove'>Remove</button>
+    <button data-id=${id} id='remove'>Remove</button>
     `;
-    return book;
+    return newbook;
+  }
+
+  AddBook(newbooks) {
+    this.books.push(newbooks);
+    localStorage.setItem(bookskey, JSON.stringify(this.books));
+
+    this.displayBooks();
+    form.reset();
+  }
+
+  removeBook() {
+    const removeBtns = document.querySelectorAll('#remove');
+
+    removeBtns.forEach((removeBtn) => {
+      removeBtn.addEventListener('click', (e) => {
+        const { id } = e.target.dataset;
+        this.books = this.books.filter((book) => book.id !== +id);
+        localStorage.setItem(bookskey, JSON.stringify(this.books));
+        this.displayBooks();
+      });
+    });
   }
 }
 
-const removeBook = (deleteBtns) => {
-  const renderBooks = (books) => {
-    container.innerHTML = '';
-    books.forEach((book) => {
-      const { title, author, id } = book;
+window.addEventListener('DOMContentLoaded', () => {
+  const storeBookLocally = localStorage.getItem(bookskey)
+    ? JSON.parse(localStorage.getItem(bookskey)) : [];
 
-      const instanceBook = new Book(title, author, id);
-      const createdBookElement = instanceBook.AddBook();
-      container.appendChild(createdBookElement);
-    });
-    // Remove
-    deleteBtns = document.querySelectorAll('#remove');
-    removeBook(deleteBtns);
-  };
-  deleteBtns.forEach((btn) => {
-    btn.addEventListener('click', (e) => {
-      const storeBookLocally = JSON.parse(localStorage.getItem(bookskey));
-      const bookId = Number(e.target.getAttribute('data-id'));
-      const filterBooks = storeBookLocally.filter((book) => book.id !== bookId);
-      renderBooks(filterBooks);
+  const book = new Book(storeBookLocally);
+  book.displayBooks();
 
-      localStorage.setItem(bookskey, JSON.stringify(filterBooks));
-    });
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const bookData = {
+      title: titleInput.value,
+      author: authorInput.value,
+      id: Date.now(),
+    };
+    book.AddBook(bookData);
   });
-
-  window.addEventListener('DOMContentLoaded', () => {
-    const storeBookLocally = localStorage.getItem(bookskey)
-      ? JSON.parse(localStorage.getItem(bookskey)) : [];
-    renderBooks(storeBookLocally);
-  });
-};
-
-form.addEventListener('submit', (e) => {
-  e.preventDefault();
-  const storeBookLocally = JSON.parse(localStorage.getItem(bookskey));
-  const Title = titleInput.value;
-  const Author = authorInput.value;
-  const last = storeBookLocally[storeBookLocally.length - 1];
-  const id = last ? last.id + 1 : 1;
-  const book = new Book(Title, Author, id);
-
-  const createSingleBook = book.AddBook();
-  const newsBooks = [...storeBookLocally];
-  newsBooks.push(book);
-  localStorage.setItem(bookskey, JSON.stringify(newsBooks));
-  container.appendChild(createSingleBook);
-  form.reset();
-  deleteBtns = document.querySelectorAll('#remove');
-  removeBook(deleteBtns);
 });
