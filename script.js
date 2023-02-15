fab18bbb91041e00ae9a4337e9bccd54234090bc
Kagheni <Kaghenimbale@gmail.com>
2023-02-15 10:43:14 +0200

    Add book class

--- script.js
+++ script.js
@@ -4,3 +4,29 @@ const form = document.getElementById('form');
 const container = document.getElementById('container');
 const bookskey = 'books';
 let deleteBtns = document.querySelectorAll('#remove');
+
+class Book {
+  constructor (title, author, id) {
+    this.author = author;
+    this.title = title;
+    if(id) {
+      this.id = id;
+    }
+    else {
+      this.id = 1;
+    }
+  }
+
+  AddBook (){
+    const book = document.createElement('li');
+    book.innerHTML = `
+    <div class="description">
+    <h2>${this.title}</h2>
+    by
+    <h3>${this.author}</h3>
+    </div>
+    <button data-id=${this.id} id='remove'>Remove</button>
+    `;
+    return book;
+  }
+}
