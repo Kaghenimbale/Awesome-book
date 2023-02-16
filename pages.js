4e6e063df4a8cc9e50552f1fdb4eab8d0ced1512
Musong Ernest Akeh <ernestakeh@gmail.com>
2023-02-16 10:26:56 +0100

    Fix add and remove functionality

--- pages.js
+++ pages.js
@@ -5,22 +5,25 @@ const list = document.getElementById('list');
 const addNew = document.getElementById('add-new');
 const newContact = document.getElementById('contact');
 
-window.addEventListener('DOMContentLoaded', () => {
-  contactSection.classList.add('display');
-  newBookSection.classList.add('display');
-});
+// window.addEventListener('DOMContentLoaded', () => {
+//   contactSection.classList.add('display');
+//   newBookSection.classList.add('display');
+// });
 
 list.addEventListener('click', () => {
-  contactSection.classList.add('display');
-  newBookSection.classList.add('display');
+  booksSection.classList.remove('display')
+  contactSection.classList.add('display')
+  newBookSection.classList.add('display')
 });
 
 addNew.addEventListener('click', () => {
-  contactSection.classList.add('display');
-  booksSection.classList.add('display');
+  newBookSection.classList.remove('display')
+  booksSection.classList.add('display')
+  contactSection.classList.add('display')
 });
 
 newContact.addEventListener('click', () => {
-  booksSection.classList.add('display');
-  newBookSection.classList.add('display');
+  contactSection.classList.remove('display')
+  newBookSection.classList.add('display')
+  booksSection.classList.add('display')
 });
