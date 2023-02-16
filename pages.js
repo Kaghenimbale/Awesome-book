33023c5780301ce29dc12ef8f41951fc8845b1fe
Kagheni <Kaghenimbale@gmail.com>
2023-02-16 12:06:31 +0200

    Add style to center every page content

--- pages.js
+++ pages.js
@@ -8,24 +8,23 @@ const newContact = document.getElementById('contact');
 window.addEventListener('DOMContentLoaded', () => {
   contactSection.classList.add('display');
   newBookSection.classList.add('display');
-  booksSection.classList.remove('display')
+  booksSection.classList.remove('display');
 });
 
 list.addEventListener('click', () => {
-  booksSection.classList.remove('display')
-  contactSection.classList.add('display')
-  newBookSection.classList.add('display')
+  booksSection.classList.remove('display');
+  contactSection.classList.add('display');
+  newBookSection.classList.add('display');
 });
 
 addNew.addEventListener('click', () => {
-  newBookSection.classList.remove('display')
-  booksSection.classList.add('display')
-  contactSection.classList.add('display')
+  newBookSection.classList.remove('display');
+  booksSection.classList.add('display');
+  contactSection.classList.add('display');
 });
 
 newContact.addEventListener('click', () => {
-  contactSection.classList.remove('display')
-  newBookSection.classList.add('display')
-  booksSection.classList.add('display')
+  contactSection.classList.remove('display');
+  newBookSection.classList.add('display');
+  booksSection.classList.add('display');
 });
-
