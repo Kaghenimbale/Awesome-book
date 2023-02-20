bedb564d8dc1e3b4988e0cd57c76a7cb66bd3693
Kagheni <Kaghenimbale@gmail.com>
2023-02-20 13:31:26 +0200

    Import files from modules folder

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
+import { renderDomContentDb } from './module/script.js';
+import {
+  renderPageContent, renderListBook, renderContactform, renderContactSection,
+} from './module/pages.js';
+import renderDate from './module/date.js';
 
+const list = document.getElementById('list');
+const addNew = document.getElementById('add-new');
+const newContact = document.getElementById('contact');
+
+requestAnimationFrame(renderDate);
+window.addEventListener('DOMContentLoaded', renderDomContentDb);
+window.addEventListener('DOMContentLoaded', renderPageContent);
+list.addEventListener('click', renderListBook);
+addNew.addEventListener('click', renderContactform);
+newContact.addEventListener('click', renderContactSection);
