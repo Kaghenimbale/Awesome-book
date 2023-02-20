10448770cf4afd4478d59e02cae7108bb8c1144c
Kagheni <Kaghenimbale@gmail.com>
2023-02-20 13:30:39 +0200

    Add export to pages file

--- module/pages.js
+++ module/pages.js
@@ -1,22 +1,26 @@
 const newBookSection = document.querySelector('.new-book-section');
 const contactSection = document.getElementById('contact-section');
 const booksSection = document.getElementById('books-section');
-const renderListBook = () => {
+
+export const renderListBook = () => {
   booksSection.classList.remove('display');
   contactSection.classList.add('display');
   newBookSection.classList.add('display');
 };
-const renderContactform = () => {
+
+export const renderContactform = () => {
   newBookSection.classList.remove('display');
   booksSection.classList.add('display');
   contactSection.classList.add('display');
 };
-const renderContactSection = () => {
+
+export const renderContactSection = () => {
   contactSection.classList.remove('display');
   newBookSection.classList.add('display');
   booksSection.classList.add('display');
 };
-const renderPageContent = () => {
+
+export const renderPageContent = () => {
   contactSection.classList.add('display');
   newBookSection.classList.add('display');
   booksSection.classList.remove('display');
